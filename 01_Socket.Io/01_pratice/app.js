114830a285a9ce2a7151e913211cbd6dab14020a
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-03-15 10:30:03 +0530

    Socket.Io 1

--- 01_Socket.Io/01_pratice/app.js
+++ 01_Socket.Io/01_pratice/app.js
@@ -3,6 +3,8 @@ const http = require('http').Server(app);
 
 const path = require("path");
 
+const io = require("socket.io")(http);
+
 app.get('/', function (req, res) {
 
     let options = {
@@ -11,7 +13,16 @@ app.get('/', function (req, res) {
     let fileName = 'index.html';
 
     res.sendFile(fileName, options);
-})
+});
+
+io.on('connection', function(socket) {
+    console.log("User connected");
+
+    socket.on('disconnect', function() {
+        console.log("User disconnected");
+    });
+    
+});
 
 http.listen(5000, function () {
     console.log("Server running on 5000");
